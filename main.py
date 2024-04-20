f47dc227025d6102436a8ea1dd7fbe28da35a881
tonlls <tonlls1999@gmail.com>
2024-04-20 04:46:02 +0200

    everising

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI(title="LleidaHack API",
 
 app.add_middleware(
             CORSMiddleware,
-            allow_origins=["localhost"],
+            allow_origins=["*"],
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
